8e9c8bd29e1ab5203f39c5f1e5031ddcc4150bbc
673317908 <673317908@qq.com>
2021-01-24 00:54:54 +0800

    重置样式、固定样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 重置样式
+import "./styles/reset.css";
+import "./styles/index.min.css";
 
 Vue.config.productionTip = false;
 
