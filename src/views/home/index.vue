d2fc9a9bd9a64a89945f640750f3e598e056782b
673317908 <673317908@qq.com>
2021-01-24 00:53:37 +0800

    主页框架

---
+++ src/views/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header />
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "../../components/header/index";
+import Footer from "../../components/footer/index";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header,Footer
+  },
+};
+</script>
+
+<style scoped>
+</style>
