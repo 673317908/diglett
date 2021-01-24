87bd27b1bd3bc876d2900ace23870dcbc592ef0d
673317908 <673317908@qq.com>
2021-01-24 23:41:06 +0800

    安装引入element-ui

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import store from "./store";
 // 重置样式
 import "./styles/reset.css";
 import "./styles/index.min.css";
+// ui库
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
