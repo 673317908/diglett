be90bc6611cf794644cc8b6f98023724c10c9283
673317908@qq.com <673317908@qq.com>
2021-01-28 20:08:36 +0800

    完成网站开发静态

---
+++ src/views/web/index.vue
@@ -0,0 +1,377 @@
+<template>
+  <div>
+    <div class="banner">
+      <img src="../../assets/images/web_slices/tuceng1@2x.png" alt="" />
+    </div>
+    <div class="web">
+      <div class="web_title text_c">
+        <p class="fs_36 fw_700 ma_b_28">网站开发</p>
+        <p class="fs_24">WEB DEVELOPMENT</p>
+      </div>
+      <div class="web_list">
+        <div class="web_item ps_r">
+          <img
+            src="../../assets/images/web_slices/setuwang_401344326@2x.png"
+            alt=""
+          />
+          <div class="web_desc ps_a">
+            <webTitle title="集团站群" />
+            <div class="web_desc_tips ma_t_26 ma_b_40">
+              <p class="fs_24 text_over text_over_1">
+                案例：中国南山、广百荟、白马服装城
+              </p>
+            </div>
+            <div class="web_desc_content">
+              <p class="fs_24">
+                采用“主站+分站”的模式，主站可数据统一管理，
+                每个分站均属于独立的系统，可设立独立的数据库 、顶级域名、不
+                同的服务器。系统拥有高安全、模 块化核心优势。
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="web_item2 flex al_c jcc">
+          <div class="web_item2_l">
+            <webTitle title="品牌网站展示" />
+            <div class="web_desc_tips ma_t_26 ma_b_40">
+              <p class="fs_24 text_over text_over_1">
+                案例：依吉谛、杰睿亚、沃安
+              </p>
+            </div>
+            <div class="web_desc_content">
+              <p class="fs_24">
+                提升品牌价值和市场占有率为目标。充分切入市场，
+                洞察品牌核心，围绕着品牌定位与目标受众的诉求
+                进行分析，寻找品牌的独特记忆点，根据其调性提
+                炼设计方向，力求通过视觉感官的冲击令目标受众
+                对品牌产生深刻的印象，从而强化品牌认知，扩大 知名度和影响力。
+              </p>
+            </div>
+          </div>
+          <div class="web_item2_r ma_l_36">
+            <img src="../../assets/images/web_slices/tuceng567@2x.png" alt="" />
+          </div>
+        </div>
+        <div class="web_item ps_r">
+          <img src="../../assets/images/web_slices/tuceng7@2x.png" alt="" />
+          <div class="web_desc ps_a">
+            <webTitle title="官方门户网站" />
+            <div class="web_desc_tips ma_t_26 ma_b_40">
+              <p class="fs_24 text_over text_over_1">
+                案例：沃安集团、广州建筑集团、广百集团等
+              </p>
+            </div>
+            <div class="web_desc_content">
+              <p class="fs_24">
+                官方多用于集团性企事业单位，是其对外宣传交流
+                的窗口。黑蜂科技，根据集团的企业文化和行业调
+                性，给出贴合集团的设计方案。网站页面清晰流畅，
+                功能布局调控合理，让集团信息能快速触达用户， 展示集团实力。
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="web_item2 flex al_c jcc">
+          <div class="web_item2_l">
+            <webTitle title="企业营销网站" />
+            <div class="web_desc_tips ma_t_26 ma_b_40">
+              <p class="fs_24 text_over text_over_1">
+                案例：缔惠物流、三江侗网、白云山和黄、车天车地等。
+              </p>
+            </div>
+            <div class="web_desc_content">
+              <p class="fs_24">
+                以现代营销理念为核心，从用户角度出发，通过对网
+                站架构和页面的精心布局，突显企业产品和服务，从
+                而帮助企业拓宽获客渠道，提高客户转化率，赢取订
+                单。自主和灵活是黑蜂科技网站设计的两大特点，帮
+                助企业实现精准营销。
+              </p>
+            </div>
+          </div>
+          <div class="web_item2_r ma_l_36">
+            <img src="../../assets/images/web_slices/tuceng566@2x.png" alt="" />
+          </div>
+        </div>
+        <div class="web_item ps_r">
+          <img
+            src="../../assets/images/web_slices/setuwang_400631243@2x.png"
+            alt=""
+          />
+          <div class="web_desc ps_a">
+            <div class="web_desc_tips ma_t_26 ma_b_40">
+              <p class="fs_24 text_over text_over_1">
+                案例：茉苛官网、广百荟、白马服装城、歌莉
+              </p>
+            </div>
+            <div class="web_desc_content">
+              <p class="fs_24">
+                互联网+解决方案的发展让电子商务迎来全新时代。
+                黑蜂科技为企业提供O2O、B2B、B2C、C2C、
+                B2B2C等多种电商平台建设，以创意设计为核心，
+                打造有视觉冲击力的网站，提高留存复购率。帮 +
+                助企业整合线下资源，融合线上平台，实现业务双 线布局。
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="web_item2 flex al_c jcc">
+          <div class="web_item2_l">
+            <webTitle title="信息门户网站" />
+            <div class="web_desc_tips ma_t_26 ma_b_40">
+              <p class="fs_24 text_over text_over_1">
+                案例：众享吧、南方能源观察、广州商业总会、赛立
+              </p>
+            </div>
+            <div class="web_desc_content">
+              <p class="fs_24">
+                是集合某个行业信息流的一站式平台，多被事业单位，
+                行业，地方，协会等应用。 架构+运营+盈利（服务）
+                是门户类网站核心。黑蜂科技凭借多年建站经验，对信
+                息展示进行了重点细分，让浏览网站的用户用最短时间
+                获取资讯，展现门户风采。
+              </p>
+            </div>
+          </div>
+          <div class="web_item2_r ma_l_36">
+            <img src="../../assets/images/web_slices/tuceng4@2x.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 推荐案例 -->
+    <div class="recommend">
+      <div class="recommend_box mini_w pad_b_50">
+        <Title :titleSetting="recommendTitle" />
+        <div class="recommend_list flex fl_w ma_t_50">
+          <div
+            class="recommend_item ma_r_8 ma_b_28 text_c pad_14 ps_r"
+            v-for="item in recommendArr"
+            :key="item.id"
+          >
+            <img :src="item.imgUrl" alt />
+            <p class="fs_20 fw_700 ma_t_16">{{ item.title }}</p>
+          </div>
+        </div>
+        <div class="recommend_more text_c fs_42">+</div>
+      </div>
+    </div>
+    <!-- 业务展示 -->
+    <div class="show">
+      <div class="show_box mini_w">
+        <Title :titleSetting="showTitle" />
+        <div class="show_list ma_t_46 flex">
+          <div
+            class="show_item ma_r_10 pad_t_24 pad_r_50 pad_l_50"
+            v-for="item in 5"
+            :key="item"
+          >
+            <div class="show_img">
+              <img src="../../assets/images/wechat_slices/zu59@2x.png" alt />
+            </div>
+            <div class="show_title pad_b_16 ps_r text_c">
+              <p class="fs_28 fw_700">网站建设</p>
+            </div>
+            <div class="show_path text_c">
+              <p class="fs_18 ma_b_17">品牌展示网站</p>
+              <p class="fs_18 ma_b_17">品牌展示网站</p>
+              <p class="fs_18 ma_b_17">品牌展示网站</p>
+              <p class="fs_18 ma_b_17">品牌展示网站</p>
+              <p class="fs_18 ma_b_17">品牌展示网站</p>
+              <p class="fs_18 ma_b_17">品牌展示网站</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from "../../components/title/title";
+import webTitle from "./components/title";
+export default {
+  components: {
+    Title,
+    webTitle,
+  },
+  data() {
+    return {
+      recommendTitle: {
+        title: "推荐案例",
+        tips: "RECOMMENDED CASE",
+        titleColor: "white",
+        tipsColor: "white",
+      },
+      recommendArr: [
+        {
+          id: 1,
+          imgUrl: require("../../assets/images/home_slices/shejiaoAPP-Bene.png"),
+          title: "社交APP-Bene社交",
+        },
+        {
+          id: 2,
+          imgUrl: require("../../assets/images/home_slices/zufangAPP.png"),
+          title: "租房APP-租客",
+        },
+        {
+          id: 3,
+          imgUrl: require("../../assets/images/home_slices/jiaoyuAPP.png"),
+          title: "教育APP-腾讯课堂",
+        },
+        {
+          id: 4,
+          imgUrl: require("../../assets/images/home_slices/jinrongAPP.png"),
+          title: "金融APP-优财金融",
+        },
+        {
+          id: 5,
+          imgUrl: require("../../assets/images/home_slices/dianshangAPP.png"),
+          title: "电商APP-轻拍购物",
+        },
+        {
+          id: 6,
+          imgUrl: require("../../assets/images/home_slices/canyinAPP.png"),
+          title: "餐饮APP-米熊",
+        },
+      ],
+      showTitle: {
+        title: "业务展示",
+        tips: "Business presentation",
+        titleColor: "#000000",
+        tipsColor: "#666666",
+      },
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+.banner {
+  img {
+    width: 100%;
+  }
+}
+.web {
+  background: white;
+  padding-top: 63px;
+  // padding-bottom: 117px;
+  .web_title {
+    p:nth-child(2) {
+      color: #666666;
+    }
+  }
+  .web_list {
+    margin-top: 117px;
+    .web_item {
+      img {
+        width: 100%;
+      }
+      .web_desc {
+        top: 50%;
+        right: 13%;
+        transform: translateY(-50%);
+        .web_desc_content {
+          color: #666666;
+          width: 535px;
+        }
+      }
+    }
+    .web_item2 {
+      .web_desc_content {
+        color: #666666;
+        width: 582px;
+      }
+    }
+  }
+}
+.recommend {
+  background: url("../../assets/images/web_slices/ff858317302cb0bc38bf40886ab5032a@2x.png")
+    no-repeat center center;
+  background-size: cover;
+  .recommend_box {
+    padding-top: 40px;
+    .recommend_item {
+      border: 1px solid #dfdfdf;
+      width: 388px;
+      background: white;
+      z-index: 99;
+      img {
+        width: 360px;
+        margin: 0 auto;
+      }
+      &::after {
+        width: 100%;
+        content: "";
+        height: 6px;
+        background: #00a0e9;
+        left: 0;
+        bottom: -1px;
+        display: none;
+        position: absolute;
+      }
+      &:hover::after {
+        display: block;
+      }
+    }
+    .recommend_more {
+      width: 74px;
+      height: 74px;
+      border-radius: 50%;
+      line-height: 66px;
+      font-family: SourceHanSansCN-Normal;
+      margin: 0 auto;
+      user-select: none;
+      cursor: pointer;
+      background: #00a0e9;
+      color: white;
+    }
+  }
+}
+.show {
+  padding-top: 75px;
+  padding-bottom: 88px;
+  .show_item {
+    width: 232px;
+    background: #f2f2f2;
+    &:hover {
+      outline: 2px solid #00a0e9;
+      box-shadow: 0px 1px 17px 1px rgba(0, 37, 54, 0.28);
+    }
+    .show_img {
+      width: 132px;
+      margin: 0 auto 12px auto;
+      img {
+        width: 100%;
+      }
+    }
+    .show_title {
+      &::after {
+        content: "";
+        display: block;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 42px;
+        height: 3px;
+        background: #888888;
+      }
+    }
+    .show_path {
+      color: #555555;
+      margin-top: 66px;
+      p {
+        cursor: pointer;
+        user-select: none;
+      }
+      p:hover {
+        color: #00a0e9;
+      }
+    }
+  }
+}
+</style>
