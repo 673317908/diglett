819ab5f5faba57de811a2719db339f2b8ad96c65
673317908@qq.com <673317908@qq.com>
2021-01-28 20:14:35 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -31,13 +31,58 @@ const routes = [
         name: "News",
         component: () =>
           import("../views/news/index.vue")
+      },
+      {
+        path: "/app",
+        name: "App",
+        component: () => import("../views/app/index.vue")
+      },
+      {
+        path: "/wechat",
+        name: "Wechat",
+        component: () => import("../views/wechat/index.vue")
+      },
+      {
+        path: "/about",
+        name: "About",
+        component: () => import("../views/about/index.vue")
+      },
+      {
+        path: "/culture",
+        name: "Culture",
+        component: () => import("../views/culture/index.vue")
+      },
+      {
+        path: "/contact",
+        name: "Contact",
+        component: () => import("../views/contact/index.vue")
+      },
+      {
+        path: "/web",
+        name: "Web",
+        component: () => import("../views/web/index.vue")
+      },
+      {
+        path: "/system",
+        name: "System",
+        component: () => import("../views/system/index.vue")
+      },
+      {
+        path: "/solve",
+        name: "Solve",
+        component: () => import("../views/solve/index.vue")
       }
     ]
   },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  // 指定该钩子函数，返回坐标值即可
+  scrollBehavior(to, from, savedPosition) {
+
+    return { x: 0, y: 0 }
+  }
 });
 
 export default router;
