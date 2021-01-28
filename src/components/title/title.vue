5af544f95bb60c6003039f3612e5bb47dd11b222
673317908@qq.com <673317908@qq.com>
2021-01-28 20:08:07 +0800

    制作标题组件

---
+++ src/components/title/title.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="title_box mini_w text_c">
+      <p
+        class="fs_36 fw_700 ma_b_12"
+        :style="{'--color':titleSetting.titleColor}"
+      >{{titleSetting.title}}</p>
+      <p
+        class="fs_24"
+        :style="{'--color':titleSetting.tipsColor}"
+      >{{titleSetting.tips}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    titleSetting: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="less">
+.title_box {
+  font-family: MicrosoftYaHei;
+  z-index: 99;
+  p:nth-child(1) {
+    color: var(--color);
+  }
+  p:nth-child(2) {
+    color: var(--color);
+  }
+}
+</style>
---
+++ src/views/system/components/title.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="web_desc_title ps_r">
+      <p class="fs_30 fw_700">{{ title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+.web_desc_title {
+  p {
+    color: #00a0e9;
+  }
+  &::after {
+    display: block;
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -18px;
+    width: 67px;
+    height: 6px;
+    background: #00a0e9;
+  }
+}
+</style>
