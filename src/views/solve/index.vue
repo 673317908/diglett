39d445ad615c303c39132adb799449eb5c1a3ab9
673317908@qq.com <673317908@qq.com>
2021-01-28 20:09:15 +0800

    完成解决方案静态

---
+++ src/views/solve/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <div class="banner">
+        <img src="../../assets/images/app_slices/tuceng565@2x.png" alt="">
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="less">
+.banner {
+  img {
+    width: 100%;
+  }
+}
+</style>
