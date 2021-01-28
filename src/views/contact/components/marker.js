0abad414acebf08d9a8f0d781e8b9e590ba06011
673317908@qq.com <673317908@qq.com>
2021-01-28 20:12:34 +0800

    制作地图组件

---
+++ src/views/contact/components/marker.js
@@ -0,0 +1,19 @@
+let marker = null;
+export function amapSetMarker(lnglat, map) {
+    if (marker) {
+        map.remove(marker);
+        marker = null;
+    }
+    marker = new AMap.Marker({
+        position: [lnglat.lng, lnglat.lat]
+    })
+    map.add(marker);
+    map.setFitView()
+}
+
+export function amapClearMarker(map) {
+    if (marker) {
+        map.remove(marker);
+        marker = null;
+    }
+}
